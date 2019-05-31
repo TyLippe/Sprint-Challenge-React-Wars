d725a1ef9c835bf8adf9f736ee86caebfae671c5
Ty Lippe <tylippe94@gmail.com>
2019-05-31 13:01:04 -0400

    styles added

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -11,28 +11,26 @@ class Characters extends React.Component {
           
           <h3>{this.props.starWarsProps.name}</h3>
           <p>
-            <strong>Home World:</strong> {this.props.starWarsProps.homeworld}
+            <strong>Gender:</strong> {this.props.starWarsProps.gender.charAt(0).toUpperCase() + this.props.starWarsProps.gender.slice(1)}
           </p>
           <p>
-            <strong>Birth Year:</strong> {this.props.starWarsProps.birth_year}
+            <strong>Birth Year:</strong> {this.props.starWarsProps.birth_year.charAt(0).toUpperCase() + this.props.starWarsProps.birth_year.slice(1)}
           </p>
           <p>
-            <strong>Gender:</strong> {this.props.starWarsProps.gender}
+            <strong>Height:</strong> {this.props.starWarsProps.height.charAt(0).toUpperCase() + this.props.starWarsProps.height.slice(1)}
           </p>
           <p>
-            <strong>Hair Color:</strong> {this.props.starWarsProps.hair_color}
+            <strong>Mass:</strong> {this.props.starWarsProps.mass.charAt(0).toUpperCase() + this.props.starWarsProps.mass.slice(1)}
           </p>
           <p>
-            <strong>Eye Color:</strong> {this.props.starWarsProps.eye_color}
+            <strong>Skin Color:</strong> {this.props.starWarsProps.skin_color.charAt(0).toUpperCase() + this.props.starWarsProps.skin_color.slice(1)}
+          </p>
+
+          <p>
+            <strong>Hair Color:</strong> {this.props.starWarsProps.hair_color.charAt(0).toUpperCase() + this.props.starWarsProps.hair_color.slice(1)}
           </p>
           <p>
-            <strong>Species:</strong> {this.props.starWarsProps.species}
-          </p>
-          <p>
-            <strong>Starships:</strong> {this.props.starWarsProps.starships}
-          </p>
-          <p>
-            <strong>Films Appeared In:</strong> {this.props.starWarsProps.films}
+            <strong>Eye Color:</strong> {this.props.starWarsProps.eye_color.charAt(0).toUpperCase() + this.props.starWarsProps.eye_color.slice(1)}
           </p>
 
         </div>
